567c23f3bc69e1e18cac28071b5ee8bad11d01df
Nitish Khadaria <thenitishkhadaria@gmail.com>
2022-10-05 22:55:22 +0530

    FindNemo

---
+++ src/section3-bigO/FindNemo.java
@@ -0,0 +1,23 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+public class FindNemo {
+
+	public static void main(String[] args) {
+		Instant start = Instant.now(); 
+		String[] input = new String[1000];
+		Arrays.fill(input, "nemo");
+		
+		for (int i = 0; i < input.length; i++) {
+			if(input[i] =="nemo") {
+				System.out.println("Find Nemo");
+			}
+		}
+		Instant end = Instant.now();
+		Duration timeElapsed = Duration.between(start, end);
+		System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
+	
+	}
+
+}
